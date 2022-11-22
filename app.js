a556504e81ca090aac93ce18886bcfb924159562
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-11-22 12:49:49 +0545

    Simple RESTful routes

--- app.js
+++ app.js
@@ -1,21 +1,71 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const methodOverride = require('method-override')
+const { comments } = require('./data')
+let listofcomments = [...comments]
+const { v4: uuidv4 } = require('uuid');
 var bodyParser = require('body-parser')
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
 app.use(bodyParser.json())
-app.get('/', (req, res) => {
-    res.send("Welcome")
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+// Display all comments
+app.get('/comments', (req, res) => {
+    res.render("index", { listofcomments })
 })
-app.get('/tacos', (req, res) => {
-    res.send("get request working")
+// Form to create new comment
+app.get('/comments/new', (req, res) => {
+    res.render("new")
 })
-app.post('/tacos', (req, res) => {
-    console.log(req.body)
-    res.send("post request working")
+// Creates new comment on server
+app.post('/comments', (req, res) => {
+    const id = uuidv4();
+    const { username, comment } = req.body
+    listofcomments.push({ id, username, comment })
+    res.redirect('/comments')
 })
+// Details for one specific comment
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params
+    const comment = listofcomments.find((c) => {
+        return c.id === id
+    })
+    res.render('show', { comment })
+})
+// Form to edit specific comment
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = listofcomments.find((c) => {
+        return c.id === id
+    })
+    res.render('edit', { comment })
+})
+// Edit specific comment
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params
+    const newComment = req.body.comment
+    const foundcomment = listofcomments.find((c) => {
+        return c.id === id
+    })
+    foundcomment.comment = newComment
+    res.redirect('/comments')
+})
+// Delete routes
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    listofcomments = listofcomments.filter((element) => {
+        if (element.id !== id) {
+            return true
+        } else {
+            return false
+        }
+    })
+    res.redirect('/comments')
+})
+
 app.listen(3000, () => {
     console.log("Listening to port 3000")
 })
---
+++ data.js
@@ -0,0 +1,24 @@
+const { v4: uuidv4 } = require('uuid');
+
+exports.comments = [
+    {
+        id: uuidv4(),
+        username: "Anish",
+        comment: "lol that is so funny"
+    },
+    {
+        id: uuidv4(),
+        username: "Mahendra",
+        comment: "nice"
+    },
+    {
+        id: uuidv4(),
+        username: "Saroj",
+        comment: "awesome buddy"
+    },
+    {
+        id: uuidv4(),
+        username: "kamal",
+        comment: "I liked your post"
+    }
+]
